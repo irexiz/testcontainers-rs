1c817be8d5ee40ecb9b0b45da82f7a503759bf22
Ireneusz Żagan <ireneusz.zagan@playbookengineering.com>
2022-03-09 10:18:20 +0100

    add send + sync

--- src/core/container.rs
+++ src/core/container.rs
@@ -27,7 +27,7 @@ use std::{fmt, marker::PhantomData, net::IpAddr, str::FromStr};
 /// [drop_impl]: struct.Container.html#impl-Drop
 pub struct Container<'d, I: Image> {
     id: String,
-    docker_client: Box<dyn Docker>,
+    docker_client: Box<dyn Docker + Send + Sync>,
     image: RunnableImage<I>,
     command: Command,
     ports: Ports,
@@ -59,7 +59,7 @@ where
     /// [`wait_until_ready`]: trait.Image.html#tymethod.wait_until_ready
     pub(crate) fn new(
         id: String,
-        docker_client: impl Docker + 'static,
+        docker_client: impl Docker + Send + Sync + 'static,
         image: RunnableImage<I>,
         command: Command,
     ) -> Self {
